21f38047af3df11ba77cdee41db56467adf03603
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-02-01 12:32:34 +0100

    Issue Again
    Don't understand how to limit the amount of grids, why isn't it working?

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ const selectionButton = document.createElement("button")
 const container = document.querySelector(".container")
 const div = document.createElement("div")
 
+
 main.insertBefore(selectionButton, container)
 selectionButton.textContent = "Choose Size"
 div.style.backgroundColor = 'black'
@@ -11,26 +12,38 @@ div.style.flexGrow = "1"
 div.style.flexShrink = "0"
 div.style.flexBasis = "6%"
 
+function beginningLoop() {
+    for (let i = 0; i < 16 * 16; i++) {
+        let store = "";
+        store = div.cloneNode()
+        container.appendChild(store)
+    }
+}
+
+beginningLoop()
+
+function generateForAmount(x, y, referenceNode, intoWhichNode) {
+    referenceNode.style.flexBasis = `${100 / x}%`
+    for (let i = 0; i < x * y; i++) {
+        let storege = ""
+        storege = referenceNode.cloneNode()
+        intoWhichNode.appendChild(storege)
+    }
+}
+
+
 
 
 
 selectionButton.addEventListener("click", () => {
-    let eek = prompt("Grid Size?")
+    let list = container.childNodes
+    list.forEach((e) => {container.removeChild(e)})
+    let eek = prompt("X")
+    let deek = prompt("Y")
+    generateForAmount(eek, deek, div, container)
+
+
 })
-
-
-
-console.log(div)
-// Create 1 tile
-// Do this 16 x 16 times
-
-for (let i = 0; i < 16 * 16; i++) {
-    let store = "";
-    store = div.cloneNode()
-    
-    container.appendChild(store)
-}
-
 
 
 const list = container.childNodes
@@ -40,7 +53,6 @@ list.forEach((element) => {
         e.target.style.backgroundColor = "red"
     })
 }) 
-
 
 
 
