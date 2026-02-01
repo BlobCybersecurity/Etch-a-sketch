33e9cf32955cce788aa03916186fb4f7d80c0e90
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-02-01 10:22:08 +0100

    Grid Selection 1 problem
    How do i make the grid selection button be ontop...

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
+const main = document.querySelector(".main")
+const selectionButton = document.createElement("button")
 const container = document.querySelector(".container")
 const div = document.createElement("div")
+
+main.appendChild(selectionButton)
+selectionButton.textContent = "Choose Size"
 div.style.backgroundColor = 'black'
 div.style.width = "200px"
 div.style.height = "100px"
 div.style.width = "6%"
+
+selectionButton.addEventListener("click", () => {
+    let eek = prompt("Grid Size?")
+})
 
 
 
