d157cb244d706a88bad912a4ad926d6d3f146c2a
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-02-02 20:15:37 +0100

    Main structure
    Problem, how on earth does one make a specific "amount" of items per row, I know you can use max width maybe and flex basis, but I don't understand.

--- javascript.js
+++ javascript.js
@@ -1,58 +1,91 @@
+const { cloneElement } = require("react")
+
 const main = document.querySelector(".main")
 const selectionButton = document.createElement("button")
 const container = document.querySelector(".container")
 const div = document.createElement("div")
 
-
 main.insertBefore(selectionButton, container)
-selectionButton.textContent = "Choose Size"
+
 div.style.backgroundColor = 'black'
 div.style.height = "90px"
 div.style.flexGrow = "1"
 div.style.flexShrink = "0"
-div.style.flexBasis = "6%"
 
-function beginningLoop() {
+selectionButton.textContent = "Choose Size"
+selectionButton.style.alignSelf = "center"
+
+
+
     for (let i = 0; i < 16 * 16; i++) {
         let store = "";
         store = div.cloneNode()
         container.appendChild(store)
     }
-}
-
-beginningLoop()
-
-function generateForAmount(x, y, referenceNode, intoWhichNode) {
-    referenceNode.style.flexBasis = `${100 / x}%`
-    for (let i = 0; i < x * y; i++) {
-        let storege = ""
-        storege = referenceNode.cloneNode()
-        intoWhichNode.appendChild(storege)
-    }
-}
 
 
 
 
 
-selectionButton.addEventListener("click", () => {
-    let list = container.childNodes
-    list.forEach((e) => {container.removeChild(e)})
-    let eek = prompt("X")
-    let deek = prompt("Y")
-    generateForAmount(eek, deek, div, container)
+    selectionButton.addEventListener("click", () => {
+        console.log("Clicked")
+        container.textContent = ''
+        let GridSize = prompt("Grid Size: ")
+
+        for (let i = 0; i < GridSize; i++) {
+            for (let j = 0; j < GridSize; j++) {
+                let storage = ""
+                storage = cloneElement(div)
+                container.appendChild(storage)
+            }
+        }
+    
+        const list = container.childNodes
+        list.forEach((element) => {
+
+            element.addEventListener("mouseenter", function (e) {
+                e.target.style.backgroundColor = "red"
+            })
+        }) 
+
+    })
 
 
-})
+
+
+
 
 
 const list = container.childNodes
-list.forEach((element) => {
+    list.forEach((element) => {
 
     element.addEventListener("mouseenter", function (e) {
         e.target.style.backgroundColor = "red"
     })
-}) 
+
+})
 
 
 
+
+
+
+
+
+
+// Grid Breakdown
+
+
+// iteration 0: 1, 2, 3, x...
+
+// iteration 1: 1, 2, 3, x...
+
+// iteration x...: 1, 2, 3, x...
+
+// for (let i = 0; i < GridX; i++) {
+   
+    // for (let j = 0; j < GridX; j++) {
+            // Clone Div
+//      }
+
+// }
