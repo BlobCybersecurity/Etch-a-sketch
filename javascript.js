40fc4d06fe21c02647040b777d25e575fd0a428a
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-02-04 20:07:26 +0100

    Add random colours

--- javascript.js
+++ javascript.js
@@ -5,9 +5,18 @@ const selectionButton = document.createElement("button")
 const container = document.querySelector(".container")
 const div = document.createElement("div")
 
+  function randomRGB() {
+        return Math.floor(Math.random() * 256)
+    }
+
+    function randomCOl() {
+        return `rgb(${randomRGB()},${randomRGB()},${randomRGB()})`;
+    }
+
 main.insertBefore(selectionButton, container)
 
-div.style.backgroundColor = 'black'
+div.style.backgroundColor = "White"
+div.style.outline = "solid black"
 div.style.height = "90px"
 div.style.flexShrink = "0"
 div.style.width = `90px`
@@ -17,20 +26,23 @@ selectionButton.style.alignSelf = "center"
 
 
 
+
+
+
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
              let store = "";
 
             store = div.cloneNode()
-            store.style.width = `${(960 / 16 ) - 3}px`
-            store.style.height = `${(960 / 16 ) - 3}px`
+            store.style.width = `${(800 / 16 ) - 3}px`
+            store.style.height = `${(800 / 16 ) - 3}px`
             container.appendChild(store)
         }
        
     }
 
 
-
+  
 
 
     selectionButton.addEventListener("click", () => {
@@ -42,8 +54,8 @@ selectionButton.style.alignSelf = "center"
             for (let j = 0; j < GridSize; j++) {
                 let storage = ""
                 storage = div.cloneNode()
-                storage.style.width = `${(960 / GridSize ) - 3}px`
-                storage.style.height = `${(960 / GridSize ) - 3}px`
+                storage.style.width = `${(800 / GridSize ) - 3}px`
+                storage.style.height = `${(800 / GridSize ) - 3}px`
                 container.appendChild(storage)
             }
         }
@@ -52,7 +64,8 @@ selectionButton.style.alignSelf = "center"
         list.forEach((element) => {
 
             element.addEventListener("mouseenter", function (e) {
-                e.target.style.backgroundColor = "red"
+                let randomCol = randomCOl()
+                e.target.style.backgroundColor = `${randomCol}`
             })
         }) 
 
@@ -68,7 +81,8 @@ const list = container.childNodes
     list.forEach((element) => {
 
     element.addEventListener("mouseenter", function (e) {
-        e.target.style.backgroundColor = "red"
+        let randomCl = randomCOl()
+        e.target.style.backgroundColor = randomCl
     })
 
 })
